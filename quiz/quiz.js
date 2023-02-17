d9a6cced29c0b3bbc63ed4f42baa2ac9100018b1
NicolasMosqueda <Nicomosqueda02@gmail.com>
2023-02-16 17:46:15 -0800

    Update quiz.js

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -22,7 +22,71 @@ const questions = [
       { text: "Jim Brown", correct: false },
     ],
   },
+  {
+    question: "Which team has the most regular season wins in NFL history?",
+    answers: [
+      { text: "Green Bay Packers", correct: false },
+      { text: "Chicago Bears", correct: false },
+      { text: "New England Patriots", correct: false },
+      { text: "Dallas Cowboys", correct: true },
+    ],
+  },
+  {
+    question: "Who is the only quarterback to win a Super Bowl with two different teams?",
+    answers: [
+      { text: "Joe Montana", correct: false },
+      { text: "Peyton Manning", correct: false },
+      { text: "Brett Favre", correct: false },
+      { text: "John Elway", correct: true },
+    ],
+  },
+  {
+    question: "Who is the only player to rush for 2,000 yards in a single season?",
+    answers: [
+      { text: "Barry Sanders", correct: false },
+      { text: "Eric Dickerson", correct: true },
+      { text: "Walter Payton", correct: false },
+      { text: "Jim Brown", correct: false },
+    ],
+  },
+  {
+    question: "Which team has the longest active playoff drought (as of the end of the 2021 season)?",
+    answers: [
+      { text: "New York Jets", correct: false },
+      { text: "Cleveland Browns", correct: false },
+      { text: "Detroit Lions", correct: true },
+      { text: "Houston Texans", correct: false },
+    ],
+  },
+  {
+    question: "Who is the NFL's all-time leader in touchdown passes?",
+    answers: [
+      { text: "Brett Favre", correct: false },
+      { text: "Peyton Manning", correct: true },
+      { text: "Tom Brady", correct: false },
+      { text: "Dan Marino", correct: false },
+    ],
+  },
+  {
+    question: "Who is the only player to win five Super Bowl MVP awards?",
+    answers: [
+      { text: "Tom Brady", correct: true },
+      { text: "Joe Montana", correct: false },
+      { text: "Terry Bradshaw", correct: false },
+      { text: "Bart Starr", correct: false },
+    ],
+  },
+  {
+    question: "Which NFL team was the first to win three Super Bowls in a row?",
+    answers: [
+      { text: "Pittsburgh Steelers", correct: false },
+      { text: "Dallas Cowboys", correct: true },
+      { text: "San Francisco 49ers", correct: false },
+      { text: "New England Patriots", correct: false },
+    ],
+  }
 ];
+
 
 let currentQuestionIndex = 0;
 
